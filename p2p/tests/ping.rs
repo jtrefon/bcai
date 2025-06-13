7ea0a0a5fd0f0294ee7b51717377a952b0394424
Jacek Trefon <jack@trefon.com>
2025-06-13 17:41:52 +0400

    Switch linear regression dataset to CSV

--- p2p/tests/ping.rs
+++ p2p/tests/ping.rs
@@ -1,18 +1,18 @@
-use p2p::Node;
+use p2p::{Node, NodeEvent};
 use tokio::time::{timeout, Duration};
 
 #[tokio::test]
 async fn nodes_can_ping_end_to_end() {
-    let mut a = Node::new();
-    let mut b = Node::new();
+    let mut a = Node::new(1, 0);
+    let mut b = Node::new(1, 0);
     let addr = a.listen();
     b.dial(addr);
 
     let res = timeout(Duration::from_secs(5), async {
         loop {
             tokio::select! {
-                e = a.next_event() => if e.result.is_ok() { break },
-                e = b.next_event() => if e.result.is_ok() { break },
+                e = a.next_event() => if matches!(e, NodeEvent::Ping(_)) { break },
+                e = b.next_event() => if matches!(e, NodeEvent::Ping(_)) { break },
             }
         }
     })
